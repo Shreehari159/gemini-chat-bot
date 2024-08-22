cb76727632cb6b647afcdaaf2a01958994cd2c37
Shreehari159 <jackhari159@gmail.com>
2024-08-22 20:58:27 +0530

    Add files via upload

---
+++ image.py
@@ -0,0 +1,19 @@
+import requests
+import streamlit as st
+
+API_URL = "https://api-inference.huggingface.co/models/black-forest-labs/FLUX.1-schnell"
+headers = {"Authorization": "Bearer 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"}
+
+def query(payload):
+	response = requests.post(API_URL, headers=headers, json=payload)
+	return response.content
+image_bytes = query({
+	"inputs": st.text_input('Enter your input: '),
+})
+# You can access the image with PIL.Image for example
+import io
+from PIL import Image
+image = Image.open(io.BytesIO(image_bytes))
+
+if st.button('Generate'):
+	st.image(image)
